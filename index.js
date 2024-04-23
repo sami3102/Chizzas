1bfa4f8366448203f108043d0df7daff5aacc070
Mohammed Sami Pasha <97223241+sami3102@users.noreply.github.com>
2024-04-24 01:20:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-let p_cartvalue = document.querySelector(".cart_value");
-let value=0;
-let btn = document.querySelector("#btn");
-btn.addEventListener("click",() => {
-    console.log("Button clicked6");
-});
 
-function func1(){
-    value = value+1;
-    p_cartvalue.innerText = value;
-    console.log(value);
-    if(value>=10){
-      let btn = document.getElementById("btn");
-      btn.disabled = true;
-
-    }
-  }
